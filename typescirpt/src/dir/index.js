e42450a274811b744c3c8ded181fbf53bdc316fe
gitbu <1282185668@qq.com>
2019-12-07 08:56:33 +0800

    堆的topK应用场景

---
+++ typescirpt/src/dir/index.js
@@ -0,0 +1,3 @@
+import * as foo from './foo';
+let x = foo.SomeVar.a;
+console.log(x.count);
--- typescirpt/src/index.ts
+++ typescirpt/src/index.ts
@@ -1,3 +1,3 @@
-function eat(food) {
-  console.log('i eact', food);
-}
+import * as foo from './foo';
+let x: foo.SomeType = foo.SomeVar.a;
+console.log(x.count);
